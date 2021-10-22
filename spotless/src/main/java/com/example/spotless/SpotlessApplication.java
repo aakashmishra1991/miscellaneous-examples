131a367cb97773624ae22b8e4e214538b41bfd1e
abc <abc@example.com>
2021-10-22 14:43:52 +0530

    First commit

---
+++ spotless/src/main/java/com/example/spotless/SpotlessApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotlessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotlessApplication.class, args);
+	}
+
+}
---
+++ spotless/src/test/java/com/example/spotless/SpotlessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotless;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotlessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
